2b48992d115b61c428fcfc54a28dc27ba04463cd
Nathan Henniges <demonwolf@demonwolfdev.com>
2020-08-10 09:05:50 -0500

    Revert to 433fcb2117e36f4e6915626c5d4b442b2a224283

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,59 +5,9 @@
  ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
  */
 module.exports = {
-  theme: {
-    // compatible with @nuxtjs/color-mode
-    darkSelector: '.dark-mode',
-    extend: {
-      colors: {
-        primary: {
-          100: '#eaeaee',
-          200: '#cbcbd6',
-          300: '#acacbd',
-          400: '#6e6e8b',
-          500: '#303059',
-          600: '#2b2b50',
-          700: '#1d1d35',
-          800: '#161628',
-          900: '#0e0e1b',
-        },
-        secondary: {
-          100: '#E6F3F9',
-          200: '#C0E2F0',
-          300: '#9AD1E7',
-          400: '#4EAED6',
-          500: '#028BC4',
-          600: '#027DB0',
-          700: '#015376',
-          800: '#013F58',
-          900: '#012A3B',
-        },
-      },
-    },
-    fontFamily: {
-      roboto: ['Roboto', 'sans-serif'],
-    },
-  },
-  variants: {
-    backgroundColor: [
-      'dark',
-      'dark-hover',
-      'dark-group-hover',
-      'dark-even',
-      'dark-odd',
-      'hover',
-      'responsive',
-    ],
-    borderColor: [
-      'dark',
-      'dark-focus',
-      'dark-focus-within',
-      'hover',
-      'responsive',
-    ],
-    textColor: ['dark', 'dark-hover', 'dark-active', 'hover', 'responsive'],
-  },
-  plugins: [require('tailwindcss-dark-mode')()],
+  theme: {},
+  variants: {},
+  plugins: [],
   purge: {
     // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
     enabled: process.env.NODE_ENV === 'production',
