971a0842366c8d361d606798e0c1267aca2c3360
Nathan Henniges <demonwolf@demonwolfdev.com>
2020-08-12 22:39:44 -0500

    Dark mode test.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <body class="flex flex-col min-h-screen dark:black">
+    <body class="flex flex-col min-h-screen dark:bg-black">
       <nav
         class="flex items-center lg:justify-between justify-center flex-wrap bg-white py-4 lg:px-12 shadow border-solid border-t-2 border-blue-700"
       >
