e73b0df36c884b84684c20735701f22d4162d797
Nathan Henniges <demonwolf@demonwolfdev.com>
2020-08-18 12:40:56 -0500

    Added not logged in as a component. And added it to the pages.  Also cleaned up index.

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
-  <div v-if="$auth.loggedIn">
+  <div>
     <StreamerInfomation
+      v-if="$auth.loggedIn"
       :id="$auth.user.data[0].id"
       :profileimage="$auth.user.data[0].profile_image_url"
       :displayname="$auth.user.data[0].display_name"
@@ -11,32 +12,16 @@
       :broadcastertype="$auth.user.data[0].broadcaster_type"
       :accountype="$auth.user.data[0].type"
     />
-  </div>
-  <div v-else>
-    <template>
-      <div
-        class="py-48 md:py-64"
-        style="background: linear-gradient(90deg, #667eea 0%, #764ba2 100%);"
-      >
-        <div class="container mx-auto px-6">
-          <h2 class="text-4xl font-bold mb-2 text-white">
-            Have you ever wanted basic details on a Twitch Streamer? Are you a
-            Developer who needs a Twitch User ID?
-          </h2>
-          <h3 class="text-2xl mb-8 text-gray-200">
-            Login today to get started.
-          </h3>
-        </div>
-      </div>
-    </template>
+    <NotSignedInJumbotron v-else />
   </div>
 </template>
 <script>
 import StreamerInfomation from '~/components/StreamerInfomation.vue'
-
+import NotSignedInJumbotron from '~/components/NotSignedInJumbotron.vue'
 export default {
   components: {
     StreamerInfomation,
+    NotSignedInJumbotron,
   },
 
   async asyncData({ $axios, $auth, store }) {
